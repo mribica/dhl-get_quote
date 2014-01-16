f35c8eee15d8dcb7109b5a98a642d9ad9e93a898
Muamer Ribica <muamer.ribica@gmail.com>
2014-01-16 10:49:45 +0100

    Allow response to return multiple condition errors.

--- lib/dhl/get_quote/response.rb
+++ lib/dhl/get_quote/response.rb
@@ -1,7 +1,7 @@
 class Dhl::GetQuote::Response
   include Dhl::GetQuote::Helper
 
-  attr_reader :raw_xml, :parsed_xml, :error
+  attr_reader :raw_xml, :parsed_xml, :errors
   attr_reader :currency_code, :currency_role_type_code, :weight_charge, :total_amount, :total_tax_amount, :weight_charge_tax
 
   CURRENCY_ROLE_TYPE_CODES = %w[ BILLC PULCL BASEC INVCU ]
@@ -9,10 +9,12 @@ class Dhl::GetQuote::Response
 
   def initialize(xml="")
     @raw_xml = xml
+    @errors = []
+
     begin
       @parsed_xml = MultiXml.parse(xml)
     rescue MultiXml::ParseError => e
-      @error = e
+      @errors << e
       return self
     end
 
@@ -24,14 +26,14 @@ class Dhl::GetQuote::Response
         Dhl::GetQuote::Upstream::UnknownError.new(response_error_condition_data)
       end
     elsif condition_indicates_error?
-      @error = Dhl::GetQuote::Upstream::ConditionError.new(condition_error_code, condition_error_message)
+      @errors = create_condition_errors
     else
       load_costs(DEFAULT_CURRENCY_ROLE_TYPE_CODE)
     end
   end
 
   def error?
-    !!@error
+    !@errors.empty?
   end
 
   def load_costs(currency_role_type_code=DEFAULT_CURRENCY_ROLE_TYPE_CODE)
@@ -93,18 +95,42 @@ protected
   end
 
   def condition_indicates_error?
-    @parsed_xml["DCTResponse"]["GetQuoteResponse"] &&
-      @parsed_xml["DCTResponse"]["GetQuoteResponse"]["Note"] &&
-        @parsed_xml["DCTResponse"]["GetQuoteResponse"]["Note"]["Condition"].is_a?(Hash)
+    result = false
+    if @parsed_xml["DCTResponse"]["GetQuoteResponse"] && @parsed_xml["DCTResponse"]["GetQuoteResponse"]["Note"]
+      note = @parsed_xml["DCTResponse"]["GetQuoteResponse"]["Note"]
+
+      if note.is_a?(Array)
+        result = note.map{|condition| condition.is_a?(Hash)}.include?(true)
+      else
+        result = note["Condition"].is_a?(Hash) 
+      end
+    end
+    result
+
+    #@parsed_xml["DCTResponse"]["GetQuoteResponse"] &&
+      #@parsed_xml["DCTResponse"]["GetQuoteResponse"]["Note"] &&
+        #@parsed_xml["DCTResponse"]["GetQuoteResponse"]["Note"]["Condition"].is_a?(Hash)
   end
 
-  def condition_error_code
-    @parsed_xml["DCTResponse"]["GetQuoteResponse"]["Note"]["Condition"]["ConditionCode"]
+  def create_condition_errors
+    notes = []
+    notes << @parsed_xml["DCTResponse"]["GetQuoteResponse"]["Note"]
+    notes.flatten!
+
+    notes.map do |note|
+      error_code = note["Condition"]["ConditionCode"]
+      error_message = note["Condition"]["ConditionData"].strip
+      Dhl::GetQuote::Upstream::ConditionError.new(error_code, error_message)
+    end
   end
 
-  def condition_error_message
-    @parsed_xml["DCTResponse"]["GetQuoteResponse"]["Note"]["Condition"]["ConditionData"].strip
-  end
+  #def condition_error_code
+    #@parsed_xml["DCTResponse"]["GetQuoteResponse"]["Note"]["Condition"]["ConditionCode"]
+  #end
+
+  #def condition_error_message
+    #@parsed_xml["DCTResponse"]["GetQuoteResponse"]["Note"]["Condition"]["ConditionData"].strip
+  #end
 
   def market_services
     @market_services ||= begin
@@ -119,5 +145,4 @@ protected
     end
       # @parsed_xml["DCTResponse"]["GetQuoteResponse"]["Srvs"]["Srv"]["MrkSrv"]
   end
-
 end
